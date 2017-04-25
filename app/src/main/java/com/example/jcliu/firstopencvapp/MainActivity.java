586f654d1c171df5c2dd74e9e34a76790906c6a3
jcliu <jcliu@ncnu.edu.tw>
2017-04-25 22:29:52 +0800

    Read image from res/raw

--- app/src/main/java/com/example/jcliu/firstopencvapp/MainActivity.java
+++ app/src/main/java/com/example/jcliu/firstopencvapp/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.jcliu.firstopencvapp;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView iv;
+    Mat srcMat;
 
     private BaseLoaderCallback mOpenCVCallBack = new BaseLoaderCallback(this) {
         @Override
@@ -16,6 +25,14 @@ public class MainActivity extends AppCompatActivity {
             switch(status){
                 case LoaderCallbackInterface.SUCCESS:
                     Log.d("OpenCV", "OpenCV sucess");
+                    try{
+                        srcMat = Utils.loadResource(MainActivity.this, R.raw.cameraman);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    Bitmap bm = Bitmap.createBitmap(srcMat.cols(), srcMat.rows(), Bitmap.Config.ARGB_8888);
+                    Utils.matToBitmap(srcMat, bm);
+                    iv.setImageBitmap(bm);
                     break;
                 default:
                     super.onManagerConnected(status);
@@ -28,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        iv = (ImageView)findViewById(R.id.imageView);
     }
 
     @Override
